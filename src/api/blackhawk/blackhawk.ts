59cfbb808e0a7dfd53ee2e6b77421b112860a3f4
Joseph Geringer <joegeringer@gmail.com>
2023-07-27 08:44:03 -0500

    testing

--- src/api/blackhawk/blackhawk.ts
+++ src/api/blackhawk/blackhawk.ts
@@ -17,34 +17,36 @@ export default function handler(
     req: GatsbyFunctionRequest<ContactBody>,
     res: GatsbyFunctionResponse
 ) {
-    // const pfx = fs.readFileSync(require.resolve('./TEMP-BHN-Sandbox-DDP-US-API-CertificationService-GW.p12'));
-    const pfx = fs.readFileSync('TEMP-BHN-Sandbox-DDP-US-API-CertificationService-GW.p12');
+    const pfx = fs.readFileSync(require.resolve('./TEMP-BHN-Sandbox-DDP-US-API-CertificationService-GW.p12'));
+    // const pfx = fs.readFileSync('TEMP-BHN-Sandbox-DDP-US-API-CertificationService-GW.p12');
 
-    pem.readPkcs12(pfx, { p12Password: `${process.env.BLACKHAWK_CERTIFICATE_PASSPHRASE}` }, (err, cert) => {
-        console.log(cert);
-        res.status(200).send(cert)
+    // pem.readPkcs12(pfx, { p12Password: `${process.env.BLACKHAWK_CERTIFICATE_PASSPHRASE}` }, (err, cert) => {
+    //     console.log(cert);
+    //     res.status(200).send(cert)
 
         
 
-        // const passphrase = `${process.env.BLACKHAWK_CERTIFICATE_PASSPHRASE}`;
-        // const agent = new https.Agent({ cert, passphrase })
+        const passphrase = `${process.env.BLACKHAWK_CERTIFICATE_PASSPHRASE}`;
+        const agent = new https.Agent({ pfx, passphrase })
 
-    // axios.get(`${process.env.BLACKHAWK_URL}`, {
-    //     // httpsAgent: agent
-    // })
-    // // .then(response => response.json())
-    // .then((response) => {
-    //     // console.dir(response.data, { depth: null })
-    //     // console.log(response.data);
-    //     // res.status(200).json({
-    //     //     response
-    //     // })
-    //     res.status(200).send(response.data)
-    // }).catch(error => {
-    //     console.log(error)
-    //     res.send({error})
-    // })
-    });
+    axios.get(`${process.env.BLACKHAWK_URL}`, {
+        httpsAgent: agent
+    })
+    // .then(response => response.json())
+    .then((response) => {
+        // console.dir(response.data, { depth: null })
+        // console.log(response.data);
+        // res.status(200).json({
+        //     response
+        // })
+        res.status(200).send(response.data)
+    }).catch(error => {
+        console.log(error)
+        res.send({error})
+    })
+
+
+    // });
     
 
     // const passphrase = `${process.env.BLACKHAWK_CERTIFICATE_PASSPHRASE}`;
