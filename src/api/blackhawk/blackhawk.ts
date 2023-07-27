8ac3df10acf19365920ec8a47d0822ce93764913
Joseph Geringer <joegeringer@gmail.com>
2023-07-27 08:46:07 -0500

    testing

--- src/api/blackhawk/blackhawk.ts
+++ src/api/blackhawk/blackhawk.ts
@@ -17,7 +17,7 @@ export default function handler(
     req: GatsbyFunctionRequest<ContactBody>,
     res: GatsbyFunctionResponse
 ) {
-    const pfx = fs.readFileSync(require.resolve('./TEMP-BHN-Sandbox-DDP-US-API-CertificationService-GW.p12'));
+    const pfx = fs.readFileSync('TEMP-BHN-Sandbox-DDP-US-API-CertificationService-GW.p12');
     // const pfx = fs.readFileSync('TEMP-BHN-Sandbox-DDP-US-API-CertificationService-GW.p12');
 
     // pem.readPkcs12(pfx, { p12Password: `${process.env.BLACKHAWK_CERTIFICATE_PASSPHRASE}` }, (err, cert) => {
