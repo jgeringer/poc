5eb20595586d2779b1001229115b3c1aa4df4cbf
Joseph Geringer <joegeringer@gmail.com>
2023-07-27 07:05:01 -0500

    testing

--- src/api/blackhawk.ts
+++ src/api/blackhawk.ts
@@ -1,10 +1,12 @@
 import { GatsbyFunctionRequest, GatsbyFunctionResponse } from "gatsby"
 import 'dotenv/config'
+import { Certificate } from "crypto"
 
 import fetch from "node-fetch"
 import https from "https"
 import fs from "fs"
 import path from "path"
+import axios from "axios"
 
 interface ContactBody {
     message: string
@@ -14,20 +16,21 @@ export default function handler(
     req: GatsbyFunctionRequest<ContactBody>,
     res: GatsbyFunctionResponse
 ) {
-    const pfx = fs.readFileSync(`https://joegeringer.com/TEMP-BHN-Sandbox-DDP-US-API-CertificationService-GW.p12`);
+    const pfx = fs.readFileSync(`TEMP-BHN-Sandbox-DDP-US-API-CertificationService-GW.p12`);
     const passphrase = `${process.env.BLACKHAWK_CERTIFICATE_PASSPHRASE}`;
     const agent = new https.Agent({ pfx, passphrase })
     
-    fetch(`${process.env.BLACKHAWK_URL}`, {
-        agent
+    axios.get(`${process.env.BLACKHAWK_URL}`, {
+        httpsAgent: agent
     })
-    .then(response => response.json())
-    .then(response => {
-        console.dir(response, { depth: null })
-
-        res.status(200).json({
-            response
-        })
+    // .then(response => response.json())
+    .then((response) => {
+        // console.dir(response.data, { depth: null })
+        // console.log(response.data);
+        // res.status(200).json({
+        //     response
+        // })
+        res.status(200).send(response.data)
     }).catch(error => {
         console.log(error)
         res.send({error})
