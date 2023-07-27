d8eb8fe2e205f0c71af425c646349f687a8b0d44
Joseph Geringer <joegeringer@gmail.com>
2023-07-27 08:06:28 -0500

    testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,29 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
-import { responsiveImageSizes } from "gatsby-plugin-image/dist/src/image-utils"
-
-
+import * as React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+import { responsiveImageSizes } from "gatsby-plugin-image/dist/src/image-utils";
 
 const IndexPage: React.FC<PageProps> = () => {
-  const [data, setData] = React.useState({})
-
+  const [data, setData] = React.useState({});
 
   // fetch api/blackhawk and alert the response on click
   const clickHandler = async () => {
-    try{
-      const response = await fetch("/api/blackhawk?this-is-a-test=1")
-      const data = await response.json()
-      setData(data)
+    try {
+      const response = await fetch("/api/blackhawk");
+      const data = await response.json();
+      setData(data);
     } catch (e) {
-      alert(e)
+      alert(e);
     }
-    }
+  };
 
   return (
     <main>
-      <button onClick={clickHandler}>Click me!</button>
-      <pre>
-        {JSON.stringify(data, null, 2)}
-      </pre>
+      <button onClick={clickHandler}>Click me!!</button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Home Page</title>;
