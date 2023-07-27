25e6e63d1a4e65b0891a735ec2fdb10c74dc5160
Joseph Geringer <joegeringer@gmail.com>
2023-07-27 08:03:41 -0500

    testing

--- src/api/blackhawk.ts
+++ src/api/blackhawk.ts
@@ -7,6 +7,7 @@ import https from "https"
 import fs from "fs"
 import path from "path"
 import axios from "axios"
+import pem from 'pem'
 
 interface ContactBody {
     message: string
@@ -16,23 +17,49 @@ export default function handler(
     req: GatsbyFunctionRequest<ContactBody>,
     res: GatsbyFunctionResponse
 ) {
-    const pfx = fs.readFileSync(`${process.env.BLACKHAWK_CERTIFICATE}`);
-    const passphrase = `${process.env.BLACKHAWK_CERTIFICATE_PASSPHRASE}`;
-    const agent = new https.Agent({ pfx, passphrase })
+    const pfx = fs.readFileSync('TEMP-BHN-Sandbox-DDP-US-API-CertificationService-GW.p12');
+
+    pem.readPkcs12(pfx, { p12Password: `${process.env.BLACKHAWK_CERTIFICATE_PASSPHRASE}` }, (err, cert) => {
+        console.log(cert);
+        res.status(200).send(cert)
+
+        // const passphrase = `${process.env.BLACKHAWK_CERTIFICATE_PASSPHRASE}`;
+        // const agent = new https.Agent({ cert, passphrase })
+
+    // axios.get(`${process.env.BLACKHAWK_URL}`, {
+    //     // httpsAgent: agent
+    // })
+    // // .then(response => response.json())
+    // .then((response) => {
+    //     // console.dir(response.data, { depth: null })
+    //     // console.log(response.data);
+    //     // res.status(200).json({
+    //     //     response
+    //     // })
+    //     res.status(200).send(response.data)
+    // }).catch(error => {
+    //     console.log(error)
+    //     res.send({error})
+    // })
+    });
     
-    axios.get(`${process.env.BLACKHAWK_URL}`, {
-        httpsAgent: agent
-    })
-    // .then(response => response.json())
-    .then((response) => {
-        // console.dir(response.data, { depth: null })
-        // console.log(response.data);
-        // res.status(200).json({
-        //     response
-        // })
-        res.status(200).send(response.data)
-    }).catch(error => {
-        console.log(error)
-        res.send({error})
-    })
+
+    // const passphrase = `${process.env.BLACKHAWK_CERTIFICATE_PASSPHRASE}`;
+    // const agent = new https.Agent({ pfx, passphrase })
+    
+    // axios.get(`${process.env.BLACKHAWK_URL}`, {
+    //     httpsAgent: agent
+    // })
+    // // .then(response => response.json())
+    // .then((response) => {
+    //     // console.dir(response.data, { depth: null })
+    //     // console.log(response.data);
+    //     // res.status(200).json({
+    //     //     response
+    //     // })
+    //     res.status(200).send(response.data)
+    // }).catch(error => {
+    //     console.log(error)
+    //     res.send({error})
+    // })
 }
